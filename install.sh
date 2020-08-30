688b764123868f80c8c47b57ca81f026ff7c3f03
Vyacheslav <govras99@outlook.com>
2020-08-31 01:41:36 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,4 +4,4 @@ echo "In five seconds, we'll set up a replica."
 sleep 5
 docker exec -it mongo-cluster-1 mongo --eval "rs.initiate({ _id : 'rs0', members: [ { _id: 0, host: 'mongo1:27017' }, { _id: 1, host: 'mongo2:27017' }, { _id: 2, host: 'mongo3:27017' } ] })"
 
-echo "\nYou can connect to mongodb by this url: mongodb://`hostname -I | awk '{print $1}'`:27017/?replicaSet=rs0"
+echo "You can connect to mongodb by this url: mongodb://`hostname -I | awk '{print $1}'`:27017/?replicaSet=rs0"

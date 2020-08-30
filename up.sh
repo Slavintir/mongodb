d4c5dd9d8a19dde22e151f02474660505e326cc0
Vyacheslav <govras99@outlook.com>
2020-08-31 01:32:27 +0300

    Delete up.sh

--- up.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-docker-compose up -d
-echo "In five seconds, we'll set up a replica."
-sleep 5
-docker exec -it mongo-cluster-1 mongo --eval "rs.initiate({ _id : 'rs0', members: [ { _id: 0, host: 'mongo1:27017' }, { _id: 1, host: 'mongo2:27017' }, { _id: 2, host: 'mongo3:27017' } ] })"
-clear
-
-echo "You can connect to mongodb by this url: mongodb://`hostname -I | awk '{print $1}'`:27017/?replicaSet=rs0"
